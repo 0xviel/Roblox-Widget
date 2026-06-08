df385ae851a567573021415bd525d231d4ed6139
Dimas Saputra <ds2318014@gmail.com>
2026-06-08 21:39:27 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  clientId: 'client id here',
+  clientSecret: 'client secret here',
+  redirectUri: 'http://localhost:3000/oauth2',
+  botToken: 'bot token here',
+  discordApplicationId: 'app id here',
+  userId: 'your discord user id',
+  port: 3000,
+};
